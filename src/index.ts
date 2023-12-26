659aa493ea698a692bac1116203580730e5167ca
Oleksii Buchko <oleksiibuchko79@gmail.com>
2023-12-26 23:09:33 +0200

    added GET endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import { encrypt } from './crypto';
 import { config } from './config';
 import { readdirSync } from 'fs';
+import path from 'path';
 
 const app = express();
 
@@ -17,11 +18,50 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '30mb' }));
 app.use(bodyParser.urlencoded({ limit: '30mb' , extended: true}));
 
-app.get('/', (req: any, res: any) => {
-    const dir = readdirSync('./', { withFileTypes: true })
-        .filter(dirent => dirent.isDirectory())
-        .map(dirent => dirent.name);
-    res.status(200).send(dir);
+const directiveExceptions = [".heroku",".npm",".profile.d","dist","node_modules","src",".git",".idea"];
+const rootDir = './';
+
+app.get('/', async (req: any, res: any) => {
+  const dataArray = [];
+
+  const dirs = readdirSync('./', { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+      .filter(dirName => !directiveExceptions.includes(dirName));
+  const filteredDirs = dirs.filter(dirName => !directiveExceptions.includes(dirName));
+
+  let count = 0;
+
+  filteredDirs.forEach((folder) => {
+    const folderPath = path.join(rootDir, folder);
+
+    fs.stat(folderPath, (statErr, stats) => {
+      if (statErr) {
+        console.error('Error reading file stats:', statErr);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+      if (stats.isDirectory()) {
+        const filePath = path.join(folderPath, 'data.txt');
+
+        fs.readFile(filePath, 'utf8', (readErr, data) => {
+          if (readErr) {
+            console.error(`Error reading ${filePath}:`, readErr);
+            res.status(500).send('Internal Server Error');
+            return;
+          }
+
+          dataArray.push({ folder, data: JSON.parse(data) });
+          count++;
+
+          if (count === filteredDirs.length) {
+            res.status(200).send(dataArray);
+          }
+        });
+      }
+    });
+  });
 });
 
 app.post('/writeToFile', (req: any, res: any) => {
