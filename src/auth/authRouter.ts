2d9c117a67127c91eb9087e8bc392104a090ee32
Oleksii Buchko <oleksiibuchko79@gmail.com>
2024-01-05 19:44:15 +0200

    added session id

--- src/auth/authRouter.ts
+++ src/auth/authRouter.ts
@@ -1,40 +1,50 @@
 import express from 'express';
 import { config } from '../config';
 import axios from 'axios';
+import { randomUUID } from 'crypto';
+
+const users: { [key: string]: any } = {};
 
 const router = express.Router();
 
 router.get('/discord/login', (req, res) => {
-   const url = config.server.mode === 'prod' ? 'https://discord.com/api/oauth2/authorize?client_id=1189626660190949467&response_type=code&redirect_uri=https%3A%2F%2Foverwolf-duel-api-207077dd4a09.herokuapp.com%2Fauth%2Fdiscord%2Fcallback&scope=identify+connections'
-       : 'https://discord.com/api/oauth2/authorize?client_id=1189626660190949467&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord%2Fcallback&scope=identify+connections'
-    res.status(200).json({ url });
+   const sessionId = randomUUID();
+   users[sessionId] = null;
+   const url = config.server.mode === 'prod' ? `https://discord.com/api/oauth2/authorize?client_id=${config.discord.clientId}&response_type=code&redirect_uri=https%3A%2F%2Foverwolf-duel-api-207077dd4a09.herokuapp.com%2Fauth%2Fdiscord%2Fcallback&scope=identify+connections&state=${sessionId}`
+       : `https://discord.com/api/oauth2/authorize?client_id=${config.discord.clientId}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord%2Fcallback&scope=identify+connections&state=${sessionId}`
+    res.status(200).json({ url, sessionId });
 });
 
-router.get('/discord/token', async( req, res ) => {
-    const code = req.query.code;
-    const params = new URLSearchParams();
+// router.get('/discord/token', async( req, res ) => {
+//     const code = req.query.code;
+//     const params = new URLSearchParams();
+//
+//     params.append('client_id', config.discord.clientId);
+//     params.append('client_secret', config.discord.clientSecret);
+//     params.append('grant_type', 'authorization_code');
+//     params.append('code', code);
+//     params.append('redirect_uri', config.discord.redirectURI);
+//     const headers = {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     }
+//
+//     try {
+//         const response = await axios.post('https://discord.com/api/oauth2/token', params, { headers });
+//         res.status(200).json({ response });
+//     } catch (error) {
+//         console.log('Error', error);
+//         return res.send(error);
+//     }
+// });
 
-    let user;
-    params.append('client_id', config.discord.clientId);
-    params.append('client_secret', config.discord.clientSecret);
-    params.append('grant_type', 'authorization_code');
-    params.append('code', code);
-    params.append('redirect_uri', config.discord.redirectURI);
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
+router.get('/discord/user', async( req, res ) => {
+    const sessionId = req.query.sessionId;
+    const user = users[sessionId];
+
+    if (!user) {
+        res.status(400).send('User not found');
     }
-
-    try {
-        const response = await axios.post('https://discord.com/api/oauth2/token', params, { headers });
-        res.status(200).json({ response });
-    } catch (error) {
-        console.log('Error', error);
-        return res.send(error);
-    }
-});
-
-router.post('/discord/user', async( req, res ) => {
-    const { access_token, token_type } = req.body.data;
+    const { access_token, token_type } = user;
 
     try {
         const response = await axios.get('https://discord.com/api/users/@me',{
@@ -53,6 +63,7 @@ router.post('/discord/user', async( req, res ) => {
 router.get('/discord/callback', async( req, res ) => {
 
     const code = req.query.code;
+    const state = req.query.state;
     const params = new URLSearchParams();
 
     params.append('client_id', config.discord.clientId);
@@ -66,9 +77,11 @@ router.get('/discord/callback', async( req, res ) => {
 
     try {
         const response = await axios.post('https://discord.com/api/oauth2/token', params, { headers });
-        const { access_token, token_type } = response.data;
 
-        // await axios.get(`http://localhost:61234?access_token=${access_token}&token_type=${token_type}`);
+        if (state) {
+            users[state] = response.data;
+        }
+
         res.send(`
             <html>
               <head>
@@ -88,7 +101,7 @@ router.get('/discord/callback', async( req, res ) => {
                 </style>
               </head>
               <body>
-                <p>Now open your Overwolf Duel app</p>
+                <p>Now open your Overwolf Duel app and press Continue.</p>
               </body>
             </html>
         `);
